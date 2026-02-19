7b202f76b9c73841b2aa308114ce8d925b65603e
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-02-19 07:35:00 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -7,34 +7,34 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+first = str(first)
 
 # last
-
+last = str(last)
 
 # birth
-
+birth = int(birth)
 
 # days
-
+days = int(days)
 
 # p_first
-
+p_first = str(p_first)
 
 # p_last
-
+p_last = str(p_last)
 
 # phone
-
+phone = str(phone)
 
 # street
-
+street = str(street)
 
 # city
-
+city = str(city)
 
 # state
-
+state = str(state)
 
 # zip_code
-
+zip_code = str(zip_code)
