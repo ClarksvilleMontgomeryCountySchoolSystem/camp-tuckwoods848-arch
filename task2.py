759d6a38a1efb08821b6745a5438986065eb7c3a
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-02-23 07:46:23 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -22,28 +22,25 @@ days = int(days)
 
 # p_first
 print(type(p_first))
-p_first = str(p_first)
 
 # p_last
 print(type(p_last))
-p_last = str(p_last)
 
 # phone
 print(type(phone))
-phone = str(phone)
+
 
 # street
 print(type(street))
-street = str(street)
+
 
 # city
 print(type(city))
-city = str(city)
+
 
 # state
 print(type(state))
-state = str(state)
+
 
 # zip_code
 print(type(zip_code))
-zip_code = str(zip_code)
